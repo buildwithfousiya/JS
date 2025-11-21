091237b2977c036a4141d214e032aac672af4f42
buildwithfousiya <fousiya9198@gmail.com>
2025-11-21 20:49:40 +0530

    Practices

--- Data Types - Variables/script.js
+++ Data Types - Variables/script.js
@@ -1,5 +1,35 @@
+courseName = "Python"     //No var/let/const → becomes global variable
 function getData() {
-    courseName = "Js"
+    courseName = "Js"     // modifies the same global variable
 }
 getData();
-console.log(courseName);
+console.log(courseName); // 👉 "Js"
+
+var Name = "Python"       // declared globally
+function Data() {
+    var Name = "Js"       // declared LOCALLY (function scope)
+}
+Data();
+console.log(Name);       // 👉 "Python"
+
+// let, const, var
+// redeclaration - var 
+// reassignment - var, let 
+// var x = 10; declare
+// var x = 20; redeclare
+// x = 30 ; reassign
+
+// datatype - 8
+// primitive(store a single value) - string, number, boolean, undefined, null, bigint, symbol
+// non-primitive - object
+
+const course = {
+    name: 'Js',
+    price: 1000,
+    isAvailable: false,
+    123: 10
+};
+console.log(course);
+
+const languages = ['Malayalam', 'Hindi']
+console.log(languages[1])
