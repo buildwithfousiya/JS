a947cec83e546ebd885f44863dfece3febe1fa14
buildwithfousiya <fousiya9198@gmail.com>
2025-11-28 22:38:39 +0530

    Practices

--- Array Methods/script.js
+++ Array Methods/script.js
@@ -1,4 +1,4 @@
-const numbers = [1, 2, 3, 4, 5];
+const numbers = [1, 2, 3, 4, 5, 1];
 
 //map() is an array method used to transform each element of an array and create a new array.
 const res = numbers.map((element) => {
@@ -17,11 +17,33 @@ const ans = numbers.filter((number) => {
     if (number%2 ) //number%2 !== 0
     return true;
 });
-console.log(ans);
+console.log(ans);  //->[1, 3, 5]
 
 //Ternary Operator
 const a = numbers.filter((number) => {
 return number%2 ? true : false;
 });
-console.log(a);
+console.log(a);  //-> [1, 3, 5] 
 
+const b = numbers.filter((number => number>3));
+console.log(b);  //-> [4,5]
+
+//console.log(numbers.indexOf(1)) // get the first index 0f "1"
+const result1 = numbers.filter((value, index) => {  
+    console.log(value, index, numbers.indexOf(value));
+    return numbers.indexOf(value) === index;
+})
+console.log(result1);
+
+//reduce() in JavaScript is a higher-order array method used to reduce an array into a single value.
+result2 = numbers.reduce((accu, value) => {
+    //console.log(accu, value)
+    return "abc"
+}, "xyz");
+console.log(result2);
+
+const result3 = numbers.reduce((accu, value) => {
+    const largest = accu>value ? accu : value;
+    return largest;
+}, 0);
+console.log(result3);
---
+++ Miniproject/script.js
@@ -0,0 +1,94 @@
+const courses = [
+    {
+        name: "javaScript",
+        price: 1000,
+        rating: 4.57,
+        isAvaialable: false,
+        languages: ["Malayalam", "Tamil"],
+        mentor: { name: "abc", contact: "123-456-7890" },
+    },
+    {
+        name: "cyber Security",
+        price: 1500,
+        rating: 4.7,
+        isAvaialable: false,
+        languages: ["Malayalam", "Tamil"],
+        mentor: { name: "abc", contact: "123-456-7890" },
+    },
+    {
+        name: "node.js",
+        price: 1200,
+        rating: 4.17,
+        isAvaialable: false,
+        languages: ["Malayalam", "Tamil"],
+        mentor: { name: "abc", contact: "123-456-7890" },
+    },
+    {
+        name: "web Designing",
+        price: 1000,
+        rating: 4.8,
+        isAvaialable: false,
+        languages: ["Malayalam", "Tamil"],
+        mentor: { name: "abc", contact: "123-456-7890" },
+    },
+    {
+        name: "python",
+        price: 800,
+        rating: 4.44,
+        isAvaialable: false,
+        languages: ["Malayalam", "Tamil"],
+        mentor: { name: "abc", contact: "123-456-7890" },
+    }];
+
+const formatName = name => name.charAt(0).toUpperCase() + name.slice(1);
+
+function displayCourses(courses) {
+    const courseContainer = document.getElementById("course-container");
+    courses.forEach((course) => {
+        const courseUI = `<div class="course-card">
+        <h3>${formatName(course.name)}</h3>
+        <p>Price: ${course.price}</p>
+        <p>Rating: ${Math.round(course.rating * 10) / 10}</p>            <P>Available Languages: ${course.languages}</P>
+        <P>Instructor: ${formatName(course.mentor.name)}</P>
+        <P>Contact: ${course.mentor.contact}</P>
+        </div>`;
+        courseContainer.innerHTML += courseUI;
+    });
+}
+
+
+function findTopRatedcourse(courses){
+    const top = courses.reduce((topCourse, course) => {
+        return topCourse.rating > course.rating ? topCourse : course;
+    })
+    return top;
+}
+
+
+function displayTopRatedCourse(arr){
+    const topRatedCourse = findTopRatedcourse(arr);
+    const courseUI = `<div class="course-card">
+    <h3>${formatName(topRatedCourse.name)}</h3>
+    <p>Price: ${topRatedCourse.price}</p>
+    <p>Rating: ${Math.round(topRatedCourse.rating * 10) / 10}</p>            
+    <P>Available Languages: ${topRatedCourse.languages}</P>
+    <P>Instructor: ${formatName(topRatedCourse.mentor.name)}</P>
+    <P>Contact: ${topRatedCourse.mentor.contact}</P>
+    </div>`;
+    const topCourseContainer = document.getElementById("top-course-container");
+    topCourseContainer.innerHTML = courseUI;
+    
+}
+
+const displayCourseCount =() => {
+    const countContainer = document.getElementById("course-count");
+    countContainer.innerText = `${courses.length} courses`;
+};
+
+function initPage(){
+    displayCourseCount();
+    displayCourses(courses);
+    displayTopRatedCourse(courses);
+}
+
+initPage();
