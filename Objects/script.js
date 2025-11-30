69d944460c81b6f585af21409ffc8c99a89cd759
buildwithfousiya <fousiya9198@gmail.com>
2025-11-30 19:39:26 +0530

    Practices

--- Objects/script.js
+++ Objects/script.js
@@ -1,13 +1,15 @@
 //declaration
 
 //object literal syntax
-const user = {age: 20};
+const user = {age: 20, "food": "biriyani"};
 user.name = "abc"
 console.log(user);
 //dot notation
 console.log(user.age);
 //sqaure bracket notation
 console.log(user["age"]);
+delete user["food"]
+console.log(user);
 
 const key = "age";
 console.log(user[key]);
