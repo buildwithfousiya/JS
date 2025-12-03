005eaf1dab7209ab5fbc5ebf0fcd37e75c9d2ac2
buildwithfousiya <fousiya9198@gmail.com>
2025-12-03 21:27:57 +0530

    Practices

--- Essentials/script.js
+++ Essentials/script.js
@@ -10,7 +10,7 @@ console.log(result); //  [1, 2]
 console.log(numbers.map((num) => num * 2)); //-> [6]
 
 
-const numbers1 =  [1, 2, 3];
+const numbers1 =  [4, 5, 6];
 
 function getFirstTwoElements1(arr) {
     let copy = arr.slice(0)
@@ -21,4 +21,30 @@ const result1 = getFirstTwoElements1(numbers1);
 console.log(result1); 
 
 console.log(numbers1.map((num1) => num1 * 2)); 
+
+console.log([].concat(numbers1))         //independant copy
+ 
+console.log(Array.from(numbers1))        //independant copy
+
+console.log(Object.assign([], numbers1)) //independant copy
+
+//spread operator
+const copy = [...numbers1]
+console.log(numbers1 === copy)           //independant copy
+
+console.log([...numbers, ...numbers1])
+
+//shallow copy
+const user = {address: {pin:10}}
+const copy1 = {...user}
+copy1.address.pin = 20;
+console.log(user)
+
+//console.log(copy.adress?.pin) ->if copy.adress exists, then access copy.adress.pin.
+
+//deep copy
+const user1 = [[1, 2], [3, 4]]
+const copy2 = {...user1}
+copy2.address.pin = 20;
+console.log(user1)
  
